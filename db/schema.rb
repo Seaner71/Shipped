7ecb8f3f74cad7ec0c28af6f7d962f800079bdd9
Hillary Charen <hillary.charen@gmail.com>
2017-11-28 15:56:51 -0500

    boat location fixed paperclip again AVATAR

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128164521) do
+ActiveRecord::Schema.define(version: 20171128204851) do
 
   create_table "boats", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string "country_code"
   end
 
 end
